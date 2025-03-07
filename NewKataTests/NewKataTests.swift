f9a9bfb1c10dba105a31d94d1bed8e9de45137eb
Josh Okungbowa <josh.okungbowa@bbc.co.uk>
2025-03-07 15:51:27 +0000

    WIP

--- NewKataTests/NewKataTests.swift
+++ NewKataTests/NewKataTests.swift
@@ -7,29 +7,107 @@
 
 import XCTest
 
+
+//1️⃣ Max Profit with One Transaction
+//Problem:
+//You are given an array `prices` where `prices[i]` is the price of a given stock on the `i-th` day.
+//You want to maximize your profit by choosing one day to buy a stock and a different day in the future to sell that stock.
+//Return the maximum profit you can achieve from this transaction.
+//If you cannot achieve any profit, return `0`.
+//Example 1:
+//
+//    Input: prices = [7,1,5,3,6,4]
+//    Output: 5
+//    Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6 - 1 = 5.
+//
+//Example 2:
+//
+//    Input: prices = [7,6,4,3,1]
+//    Output: 0
+//    Explanation: No transactions can be made, so max profit = 0.
+
+
 final class NewKataTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var inputPrices: [Int]!
+    var sut: StockTradingObject!
+    var returnedProfit: Int!
+    
+    func test_givenWeekHasNoProfit_sutReturns0() {
+        givenBadWeek()
+        givenSUT()
+        whenGetMaxProfitCalled()
+        thenProfitReturned(0)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_givenWeekHasProfit_sutReturns5() {
+        givenGoodWeek()
+        givenSUT()
+        whenGetMaxProfitCalled()
+        thenProfitReturned(5)
     }
 
 }
+
+extension NewKataTests {
+    func givenBadWeek() {
+        inputPrices = [7,6,4,3,1]
+    }
+    
+    func givenGoodWeek() {
+        inputPrices = [7,1,5,3,6,4]
+    }
+    
+    func givenSUT() {
+        sut = DefaultStockTradingObject(prices: inputPrices)
+    }
+}
+
+extension NewKataTests {
+    func whenGetMaxProfitCalled() {
+        returnedProfit = sut.getMaxProfit()
+    }
+}
+
+
+extension NewKataTests {
+    func thenProfitReturned(_ expectedProfit: Int) {
+        XCTAssertEqual(returnedProfit, expectedProfit)
+    }
+}
+
+
+
+protocol StockTradingObject {
+    func getMaxProfit() -> Int
+}
+
+class DefaultStockTradingObject: StockTradingObject {
+    
+    let prices: [Int]
+    
+    init(prices: [Int]) {
+        self.prices = prices
+    }
+    
+    func getMaxProfit() -> Int {
+        var maxProfit = 0
+        guard
+            let minimumVal = prices.min(),
+            let minimumValIndex = prices.firstIndex(where: { $0 == minimumVal } )
+        else {
+            return 0
+        }
+        
+        for (index, price) in prices.enumerated() {
+            if index > minimumValIndex {
+                let delta = price - minimumVal
+                if delta > maxProfit {
+                    maxProfit = delta
+                }
+            }
+        }
+        return maxProfit
+    }
+}
+
