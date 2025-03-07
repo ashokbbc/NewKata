22e403de039562addc2c075f14093cf229517c37
ashokbbc <ashok.machineni@bbc.co.uk>
2025-03-07 11:35:39 +0000

    Initial Commit

---
+++ NewKata/NewKataApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewKataApp.swift
+//  NewKata
+//
+//  Created by Ashok Machineni on 07/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct NewKataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
